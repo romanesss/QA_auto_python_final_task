1de3222e5ba4cb0888784eca360a847ec8d13e54
Gavrilyuk Roma <romagavrilyuk2015@gmail.com>
2022-02-13 17:31:51 +0200

    Create project and add first files

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+"""
+Work with rerminal
+"""
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="fr",
+                     help="Choose language: ru or es or other")
+
+"""
+Fixture
+"""
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    """
+    Work with browser header language
+    """
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
